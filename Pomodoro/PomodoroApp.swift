a7a11d828aaeac460d2d73941af971a2c2b7a11f
Nina Zhou <nzhou@lcs.on.ca>
2024-05-24 10:55:16 -0400

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Shuyu Zhou on 2024-05-24.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
